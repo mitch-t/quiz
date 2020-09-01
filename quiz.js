0aadeb99acf0e6712749bdaca9e217efb882944a
Tom Mitchell <mitcht032@gmail.com>
2020-08-31 23:52:26 -0700

    changed the code, still needs work

--- quiz.js
+++ quiz.js
@@ -1,93 +1,139 @@
-var totalCorrect = 0;
-var totalIncorrect = 0;
-var startBtn = document.getElementById('start')
-var Intro = document.getElementById('header')
-var questionContainerElement = document.getElementById('question-container')
-let questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-buttons')
-var questions =[
-    {
-        question: 'What do you use to style a webpage?',
-        answers: [
-            {text: 'paper', correct: false},
-            {text: 'CSS', correct: true},
-            {text: 'pencils', correct: false},
-            {text: 'paint', correct: false},
-        ]
-        ,
-        question: 'Which of the following are your most likely to use to animate a webpage?',
-        answers: [
-            {text: 'javasxript', correct: true},
-            {text: 'CSS', correct: false},
-            {text: 'pencils', correct: false},
-            {text: 'paint', correct: false},
-        ]
-        ,
-        
-            question: 'What does html stand for?',
-            answers: [
-                {text: 'paper', correct: false},
-                {text: 'CSS', correct: true},
-                {text: 'pencils', correct: false},
-                {text: 'paint', correct: false},
-            ]
-    }
-]
- 
 
+const startbtn= document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+
+var scoreContainer = document.getElementById("scoreContainer");
+
+let questions =[
+        {
+        question: "What do you use to style a webpage?",
+        choiceA: "pencil",
+        choiceB: "CSS",
+        choiceC: "printer",
+        choiceD: "CEA",
+        correct: "A"
+        },
+        { 
+        question: "Which of the following are your most likely to use to animate a webpage?",
+        
+        choiceA: "javascript",
+        choiceB: "CSS",
+        choiceC: "pencils",
+        choiceD: "paint",
+        correct: "A"
+        },
+        {   
+        question: "What does html stand for?",
+            
+        choiceA: "javascript",
+        choiceB: "websites",
+        choiceC: "Hypertext Markup Langauge",
+        choiceD: "Hiddentext Magic Langauge",
+        correct: "C"
+        },
+        {
+        question: 'How do you create a function in Javascript?',
+            
+        choiceA: "function = myFunction() ",
+        choiceB: "function myFunction",
+        choiceC: "function: myFunction",
+        choiceD: "$={}",
+        correct: "B"
+        },
+        {
+        question: 'How do you write a comment in Javascript?',
+            
+        choiceA: "//this is a comment",
+        choiceB: "<!--this is a comment-->",
+        choiceC: "(this is a comment)",
+        choiceD: "just write it bro",
+        correct: "A"
+        },
+        {
+        question: 'Which event occurs when the user clicks on an HTML element?',
+            
+        choiceA: "alert",
+        choiceB: "onmouseClick",
+        choiceC: "hover",
+        choiceD: "onclick",
+        correct: "D"
+        }
+];
+ questions[0].question
+ questions[0].choiceA
+ questions[0].choiceB
+ questions[0].choiceC
+ questions[0].choiceD
+ questions[0].correct
+
+ let endQuestionIndex = questions.length-1;
+ let currentQuestionIndex = 0;
+
+
+ function renderQuestion(){
+     let q = questions[currentQuestionIndex];
+     question.innerHTML = "<p>" + q.question + "<p>";
+     choiceA.innerHTML = q.choiceA;
+     choiceB.innerHTML = q.choiceB;
+     choiceC.innerHTML = q.choiceC;
+     choiceD.innerHTML = q.choiceD;
+ }
+
+ currentQuestionIndex = 0;
+ renderQuestion()
+
+ currentQuestionIndex++
+ renderQuestion()
+
+
+ //timer
+ var questionTime = 10;
+ let count = 0;
+function timerRender(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+    }else{
+        count = 0;
+        if(currentQuestionIndex < endQuestionIndex){
+            currentQuestionIndex++;
+            renderQuestion();
+        }else{
+            clearInterval(TIMER);
+          //  scoreRender();}
+    }
+}
+let TIMER = setInterval(timerRender,1000);
+
+
+//check answer
+let score = 0;
+function checkAnswer(answer){
+    if(questions[currentQuestionIndex].correct == answer) {
+        score++;
+    }if(currentQuestionIndex < endQuestionIndex){
+        count = 0;
+        currentQuestionIndex++;
+        renderQuestion();
+    }else{
+        clearInterval(TIMER);
+    }
+}
 //calling start button to function start quiz
-startBtn.addEventListener('click',startQuiz)
+startbtn.addEventListener('click',startQuiz)
 
 // start quiz function
 function startQuiz() {
-console.log('Quiz')
+console.log('Quiz')}
 //Intro.innerHTML = "";
-//var quesions = document.createElement.innerHTML.questionContainerElement;
-
-//timer
-//var counter = 10
-//var countdown = function(){
-    //counter--;
-    //if (counter === 0){
-       // clearInterval(startCountdown);
-   // };
-//};
-//var startCountdown = setInterval(countdown, 1000);
-var questionsSize = questions.length;
-for (var i = 0; i < questionsSize; i++);
-
-//var questionContainerElement = document.createElement("question");
-//startBtn.classList.add('hide')
-//Intro.classList.add('hide')
 
 
 
-    
-    
 
-
-
-  
-
-//questionContainerElement.classList.remove('hide')
-
-
-//for(var i = 0; i < 10; i++)
-    //var question = questions [Math.floor(Math.random()
-    //* questions .length)]; }
-
-//function createNextQuestion() {
-   
-//function showQuestion()
-
-//function chosenAnswer()
-
-
-    
-
-//resetState()
-//function resetState (){
-   // while (answerButtonsElement.firstChild) {
-     //   answerButtonsElement.removeChild
-     //   (answerButtonsElement.firstChild)}}
 }
