116997b34fa707d7c96caf8275a2c8d4f029a3ea
Tom Mitchell <mitcht032@gmail.com>
2020-09-04 10:20:51 -0700

    answers dislayed

--- script.js
+++ script.js
@@ -1,76 +1,42 @@
 var startbtn= document.querySelector('#start');
 var quiz = document.getElementById("quiz");
-var question = document.getElementById("question");
+var questionELement = document.getElementById("question");
 var choiceBtnsElement =document.querySelector('#choiceBtns');
-//var choiceA = document.getElementById("btn0");
-//var choiceB = document.getElementById("btn1");
-//var choiceC = document.getElementById("btn2");
-//var choiceD = document.getElementById("btn3");
+var answerbtnsElement = document.getElementsByClassName('btns')
+var questionIndex = 0;
+var answerIndex = 0;
 
-questionsArray = [
+var questionsArray = [
     {
-    question: "What do you use to style a webpage?",
-    answer: [
-        { text:"pencil", correct: false},
-        { text:"CSS", correct: true},
-        { text:"printer", correct: false},
-        { text:"CDA", correct: false},
-        ]
+    question: 'Which of the following are you must likely to use to style a webpage?',
+    answer: ['pencil','CSS','printer','PowerPoint'],
+    correct:'CSS',
     },
-    
     { 
-    question: "Which of the following are your most likely to use to animate a webpage?",
-    
-    answer:[
-        { text:"javascript", correct: true},
-        { text:"CSS", correct: false},
-        { text:"pencil", correct: false},
-        { text:"documents", correct: false},
-        ]
+    question: 'Which of the following are you most likely to use to animate a webpage?',
+    answer:['javascript','CSS','pencil','documents'], 
+    correct: 'javascript',        
     },
-
     {   
-    question: "What does html stand for?",
-        
-    answer: [
-        { text:"javascript", correct: false},
-        { text:"Hypertext Markup Langauge", correct: true},
-        { text:"Hypertech Linguistics", correct: false},
-        { text:"Hiddentext Magic Langauge", correct: false},
-        ]
+    question: 'What does html stand for?',    
+    answer: ['javascript','Hypertext Markup Langauge','Hypertech Linguistics',"Hiddentext Magic Langauge"],
+    correct: 'Hypertext Markup Langauge',
     },
-
     {
-    question: 'How do you create a function in Javascript?',
-        
-    answer:[
-        { text:"function = myFunction()", correct: false},
-        { text:"function myFunction", correct: true},
-        { text:"function: myFunction", correct: false},
-        { text:"$={}", correct: false},
-        ]
+    question: 'How do you create a function in Javascript?',     
+    answer:['function = myFunction()','function myFunction', 'function: myFunction',"$={}"],
+    correct:'function myFunction',
     },
-
     {
-    question: 'How do you write a comment in Javascript?',
-        
-    answer:[
-        { text:"this is a comment", correct: true},
-        { text:"<!--this is a comment-->", correct: false},
-        { text:"(this is a comment)", correct: false},
-        { text:"just write it bro!", correct: false},
-        ]
+    question: 'How do you write a comment in Javascript?',    
+    answer:['//this is a comment', '<!--this is a comment-->','(this is a comment)',"just write it bro!"],
+    correct:'//this is a comment'
     },
     {
     question: 'Which event occurs when the user clicks on an HTML element?',
-        
-    answer:[
-        { text:"alert", correct: false},
-        { text:"mouseClick", correct: false},
-        { text:"hover", correct: false},
-        { text:"onclick", correct: true},
-    ]
-    }
+    answer:['alert','mouseClick', 'hover', 'click'], 
+    correct: 'click'
+    },
 ];
 
 //Timer
@@ -90,39 +56,44 @@ if(secondsLeft === 0) {
 }, 1000);
 }
 //Questions
-var questionIndex = 0;
+
+
 function generateQuestion(){
     document.querySelector('#question').textContent =
-    questionsArray[questionIndex].question;
+    questionsArray[questionIndex].question
+    document.querySelector('#choiceBtns').textContent =
+    questionsArray[answerIndex].answer
+
+    question.answer.forEach(answer=>{
+    var button = document.createElement('button')
+    button;innerText = answer.textContent
+    button.classList.add('btn')
+    if(answer.correct){
+    button.dataset.correct=answer.corrrect
+ 
+     button.addEventListener('click',selectAnswer)
+ answerbtnsElement.appendChild(button)
+   }
+} )
 }
-    //document.querySelector('#choiceBtns').textContent =
-    //questionsArray[[0]].answer;
-    //answer.forEach(answer=>{
-      //  var button = document.createElement('button')
-    //    button;innerText = answer.text
-      //  button.classList.add('btn')
-      //  if(answer.correct){
-       //     button.dataset.correct=answer.corrrect
-      //  }
-      //  button.addEventListener('click',selectAnswer)
-    //    choicebtnsElement.appendChild(button)
-    //})
-    
-//function selectAnswer(e){}
 
 function NextQuestion(){
     questionIndex++;
+    answerIndex++;
     generateQuestion();
 }
-//Choices
-function answerChoices(){
 
-}
+function selectAnswer(e){}
+//Choices
+//function answerChoices(){}
+
 
 //Where it all starts and ends
 startbtn.addEventListener('click', function(event){
     setTime();
     generateQuestion();
+    NextQuestion();
+    selectAnswer();
     removeStart(); 
 });
 
